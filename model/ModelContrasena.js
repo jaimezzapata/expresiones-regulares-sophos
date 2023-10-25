18330fc37cd456f8fb5d09bf347950e4b2e12018
jaimezzapata <zapataval2304@gmail.com>
2023-10-25 14:16:16 -0500

    Se crea modelo y controlador

---
+++ model/ModelContrasena.js
@@ -0,0 +1,30 @@
+let contrasenas = [
+    {
+        sitioWeb: 'Educacion Digital',
+        urlWeb: 'https://edigital.cesde.edu.co/login/index.php',
+        password: '123456789',
+        usuario: 'usuario',
+        descripcion: 'Sitio web institucional que permite trabajar bajo la estructura moodle'
+    },
+    {
+        sitioWeb: 'Disney+',
+        urlWeb: 'https://www.disneyplus.com/es-419/home',
+        password: 'ContraseñaDisney',
+        usuario: 'UsuarioDisney',
+        descripcion: 'Sitio de entretenimiento para ver peluculas bajo demanda'
+    },
+    {
+        sitioWeb: 'Netflix',
+        urlWeb: 'https://www.netflix.com/browse',
+        password: 'ContraseñaNetflix',
+        usuario: 'UsuarioNetflix',
+        descripcion: 'Sitio de entretenimiento para ver peluculas bajo demanda'
+    },
+    {
+        sitioWeb: 'HBO',
+        urlWeb: 'https://www.hbomax.com/co/es',
+        password: 'ContraseñaHbo',
+        usuario: 'UsuarioHbo',
+        descripcion: 'Sitio de entretenimiento para ver peluculas bajo demanda'
+    },
+]
--- view/assets/js/login.js
+++ view/assets/js/login.js
@@ -56,54 +56,6 @@ function validarCampos(e) {
 }
 
 // let btnIniciar = document.getElementById('btnIniciar')
-let btnIniciar = document.querySelector("#btnIniciar");
-btnIniciar.addEventListener("click", iniciarSesion);
-function iniciarSesion() {
-  let usuario = document.getElementById("usuario").value;
-  let contrasena = document.getElementById("contrasena").value;
-  let correo = document.getElementById("correo").value;
-  if (usuario == "Jaime") {
-    // setTimeout(function(){
-    document.getElementById("bienvenide").style.display = "flex";
-    // },1000)
-    setTimeout(() => {
-      document.getElementById("login").style.display = "none";
-      document.getElementById("panel").style.display = "flex";
-      document.getElementById("bienvenide").style.display = "none";
-    }, 3000);
-  } else {
-    alert("Error de credenciales");
-  }
-}
-
-let cerrarSesion = document.getElementById("cerrarSesion");
-cerrarSesion.addEventListener("click", cerrandoSesion);
-function cerrandoSesion() {
-  document.getElementById("login").style.display = "block";
-  document.getElementById("panel").style.display = "none";
-}
-
-let listar = document.querySelector("#listar");
-listar.addEventListener("click", listarCosas);
-function listarCosas() {
-  document.getElementById("sectionListar").style.zIndex = "3";
-  document.getElementById("sectionBuscar").style.zIndex = "2";
-  document.getElementById("sectionFiltrar").style.zIndex = "2";
-}
-let buscar = document.querySelector("#buscar");
-buscar.addEventListener("click", buscarCosas);
-function buscarCosas() {
-  document.getElementById("sectionListar").style.zIndex = "2";
-  document.getElementById("sectionBuscar").style.zIndex = "3";
-  document.getElementById("sectionFiltrar").style.zIndex = "2";
-}
-let filtrar = document.querySelector("#filtrar");
-filtrar.addEventListener("click", filtrarCosas);
-function filtrarCosas() {
-  document.getElementById("sectionListar").style.zIndex = "2";
-  document.getElementById("sectionBuscar").style.zIndex = "2";
-  document.getElementById("sectionFiltrar").style.zIndex = "3";
-}
 
 // let numeros = [1,2,3,4]
 // let letras = ['A','B','C']
