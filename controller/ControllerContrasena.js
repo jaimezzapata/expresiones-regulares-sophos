2bf261445e84567817c4c55eae65493651947cfe
jaimezzapata <zapataval2304@gmail.com>
2023-10-26 12:46:19 -0500

    Se da función al find

--- controller/ControllerContrasena.js
+++ controller/ControllerContrasena.js
@@ -34,24 +34,24 @@ function listarCosas() {
   document.getElementById("sectionBuscar").style.zIndex = "2";
   document.getElementById("sectionFiltrar").style.zIndex = "2";
   contrasenas.map((contrasena) => {
-    let sectionListar = document.getElementById('sectionListar')
+    let sectionListar = document.getElementById("sectionListar");
     let card = document.createElement("article");
     let nombre = document.createElement("h1");
     let url = document.createElement("a");
     let usuario = document.createElement("p");
     let contarsena = document.createElement("p");
     let descripcion = document.createElement("p");
-    nombre.textContent = contrasena.sitioWeb
-    url.textContent = contrasena.sitioWeb
-    url.href = contrasena.urlWeb
-    url.target = '_blanck'
-    usuario.textContent = contrasena.usuario
-    contarsena.textContent = contrasena.password
-    descripcion.textContent = contrasena.descripcion
-    card.classList.add('listadoCosas')
-    card.append(nombre,url,usuario,contarsena,descripcion)
-    console.log(card)
-    sectionListar.append(card)
+    nombre.textContent = contrasena.sitioWeb;
+    url.textContent = contrasena.sitioWeb;
+    url.href = contrasena.urlWeb;
+    url.target = "_blanck";
+    usuario.textContent = contrasena.usuario;
+    contarsena.textContent = contrasena.password;
+    descripcion.textContent = contrasena.descripcion;
+    card.classList.add("listadoCosas");
+    card.append(nombre, url, usuario, contarsena, descripcion);
+    console.log(card);
+    sectionListar.append(card);
   });
 }
 
@@ -61,16 +61,16 @@ function buscarCosas() {
   document.getElementById("sectionListar").style.zIndex = "2";
   document.getElementById("sectionBuscar").style.zIndex = "3";
   document.getElementById("sectionFiltrar").style.zIndex = "2";
-  let btnBuscar = document.getElementById('btnBuscar')
-  btnBuscar.addEventListener('click', ()=> {
-    let buscarItem = document.getElementById('buscarItem').value
-    contrasenas.find((contrasena)=>{
-        if(buscarItem == contrasena.sitioWeb){
-            console.log(contrasena)
-        }
-    })
-  })
-
+  let btnBuscar = document.getElementById("btnBuscar");
+  btnBuscar.addEventListener("click", () => {
+    let buscarItem = document.getElementById("buscarItem").value;
+    contrasenas.find((contrasena) => {
+      if (buscarItem == contrasena.sitioWeb) {
+        console.log(contrasena);
+      }
+    });
+  });
+  
 }
 let filtrar = document.querySelector("#filtrar");
 filtrar.addEventListener("click", filtrarCosas);
